703d69a043f5ae938379bb1af64bbd2659982990
Sharif Olorin <sio@tesser.org>
2016-05-30 02:55:01 +0000

    Tests for space-handling

--- test/Test/Zodiac/Request.hs
+++ test/Test/Zodiac/Request.hs
@@ -3,6 +3,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Test.Zodiac.Request where
 
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+
 import           Disorder.Core.Tripping (tripping)
 
 import           P
@@ -11,12 +14,22 @@ import           System.IO (IO)
 
 import           Test.Zodiac.Arbitrary ()
 import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
 
 import           Zodiac.Data.Request
 import           Zodiac.Request
 
 prop_tripping_CRequest :: CRequest -> Property
 prop_tripping_CRequest = tripping fromCanonicalRequest toCanonicalRequest
+
+prop_trimSpaces :: ByteString -> Property
+prop_trimSpaces bs =
+  let bs' = BS.filter (/= 0x20) bs -- no spaces
+      bs1 = trimSpaces $ " " <> bs' <> " "
+      bs2 = trimSpaces $ bs' <> "   " <> bs' in
+  case BS.null bs' of
+    True -> bs1 === bs'
+    False -> (bs1, bs2) === (bs', bs' <> " " <> bs')
 
 return []
 tests :: IO Bool
