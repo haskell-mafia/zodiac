b8f72bea929135acd6d521dd49f9d9938c66621c
Sharif Olorin <sio@tesser.org>
2016-06-07 01:42:10 +0000

    Parameterize deriveRequestKey by symmetric protocol

--- src/Zodiac/MAC.hs
+++ src/Zodiac/MAC.hs
@@ -17,22 +17,19 @@ import           Tinfoil.MAC (hmacSHA256)
 
 import           Zodiac.Data
 
-macProtocol :: SymmetricProtocol
-macProtocol = TSRPv1
-
 -- | Derive the key we actually use to sign the request from the
 -- secret key, the key ID and the date part of the request timestamp
 -- via an iterated chain of keyed hashes.
-deriveRequestKey :: RequestDate -> KeyId -> SymmetricKey -> SymmetricKey
-deriveRequestKey rd (KeyId kid) (SymmetricKey sk) =
+deriveRequestKey :: SymmetricProtocol -> RequestDate -> KeyId -> SymmetricKey -> SymmetricKey
+deriveRequestKey TSRPv1 rd (KeyId kid) (SymmetricKey sk) =
   let k0 = SymmetricKey $ sk <> renderRequestDate rd
       k1 = chainNext $ hmacSHA256 k0 prefix
       k2 = chainNext $ hmacSHA256 k1 kid
       k3 = chainNext $ hmacSHA256 k2 suffix in
   k3
   where
-    prefix = renderSymmetricProtocol macProtocol <> "-start"
+    prefix = renderSymmetricProtocol TSRPv1 <> "-start"
 
-    suffix = renderSymmetricProtocol macProtocol <> "-end"
+    suffix = renderSymmetricProtocol TSRPv1 <> "-end"
 
     chainNext = SymmetricKey . unMAC
--- test/Test/Zodiac/MAC.hs
+++ test/Test/Zodiac/MAC.hs
@@ -18,25 +18,26 @@ import           Test.QuickCheck.Instances ()
 import           Tinfoil.Data.Key (SymmetricKey(..))
 
 import           Zodiac.Data.Key
+import           Zodiac.Data.Protocol
 import           Zodiac.Data.Request
 import           Zodiac.MAC
 
-prop_deriveRequestKey_timestamp :: UniquePair RequestDate -> KeyId -> SymmetricKey -> Property
-prop_deriveRequestKey_timestamp (UniquePair rt rt') kid sk =
-  let k = deriveRequestKey rt kid sk
-      k' = deriveRequestKey rt' kid sk in
+prop_deriveRequestKey_timestamp :: SymmetricProtocol -> UniquePair RequestDate -> KeyId -> SymmetricKey -> Property
+prop_deriveRequestKey_timestamp p (UniquePair rt rt') kid sk =
+  let k = deriveRequestKey p rt kid sk
+      k' = deriveRequestKey p rt' kid sk in
   k =/= k'
 
-prop_deriveRequestKey_key_id :: RequestDate -> UniquePair KeyId -> SymmetricKey -> Property
-prop_deriveRequestKey_key_id rt (UniquePair kid kid') sk =
-  let k = deriveRequestKey rt kid sk
-      k' = deriveRequestKey rt kid' sk in
+prop_deriveRequestKey_key_id :: SymmetricProtocol -> RequestDate -> UniquePair KeyId -> SymmetricKey -> Property
+prop_deriveRequestKey_key_id p rt (UniquePair kid kid') sk =
+  let k = deriveRequestKey p rt kid sk
+      k' = deriveRequestKey p rt kid' sk in
   k =/= k'
 
-prop_deriveRequestKey_key :: RequestDate -> KeyId -> UniquePair (Blind SymmetricKey) -> Property
-prop_deriveRequestKey_key rt kid (UniquePair (Blind sk) (Blind sk')) =
-  let k = deriveRequestKey rt kid sk
-      k' = deriveRequestKey rt kid sk' in
+prop_deriveRequestKey_key :: SymmetricProtocol -> RequestDate -> KeyId -> UniquePair (Blind SymmetricKey) -> Property
+prop_deriveRequestKey_key p rt kid (UniquePair (Blind sk) (Blind sk')) =
+  let k = deriveRequestKey p rt kid sk
+      k' = deriveRequestKey p rt kid sk' in
   k =/= k'
 
 return []
