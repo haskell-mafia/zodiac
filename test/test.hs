2aee7251534a857f600f75026e2198a551ba585d
Sharif Olorin <sio@tesser.org>
2016-06-08 00:48:02 +0000

    Move http-client conversion into its own module

--- test/test.hs
+++ test/test.hs
@@ -4,6 +4,7 @@ import qualified Test.Zodiac.Data.Protocol
 import qualified Test.Zodiac.Data.Request
 import qualified Test.Zodiac.MAC
 import qualified Test.Zodiac.Request
+import qualified Test.Zodiac.Request.HttpClient
 
 main :: IO ()
 main =
@@ -12,4 +13,5 @@ main =
   , Test.Zodiac.Data.Request.tests
   , Test.Zodiac.MAC.tests
   , Test.Zodiac.Request.tests
+  , Test.Zodiac.Request.HttpClient.tests
   ]
