69579e2ec753016f2736add2725541e37e1bf82d
Sharif Olorin <sio@tesser.org>
2016-06-08 02:49:36 +0000

    TSRP: Compute MAC of a request

--- src/Zodiac/Symmetric.hs
+++ src/Zodiac/Symmetric.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Zodiac.Symmetric(
     authenticationString
+  , macRequest
   ) where
 
 import           Data.ByteString (ByteString)
@@ -11,10 +12,28 @@ import qualified Data.Text.Encoding as T
 import           P
 
 import           Tinfoil.Data (HashFunction, renderHashFunction, renderHash)
+import           Tinfoil.Data (MAC, SymmetricKey, keyHashFunction)
 import           Tinfoil.Hash (hash)
+import           Tinfoil.MAC (macBytes)
 
 import           Zodiac.Data
+import           Zodiac.MAC
 import           Zodiac.Request
+
+-- | Authenticate a 'CanonicalRequest' with a secret key.
+macRequest :: SymmetricProtocol
+           -> HashFunction
+           -> KeyId
+           -> RequestTimestamp
+           -> RequestExpiry
+           -> CRequest
+           -> SymmetricKey
+           -> MAC
+macRequest TSRPv1 hf kid rts re cr sk =
+  let authKey = deriveRequestKey TSRPv1 (timestampDate rts) kid sk
+      authString = authenticationString TSRPv1 hf kid rts re cr
+      khf = keyHashFunction hf in
+  macBytes khf authKey authString
 
 -- | Construct a string to authenticate, from all the information which will
 -- be included in the request MAC.
