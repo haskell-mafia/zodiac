87016d089db7700ddd45f50a90df35adf9eb804d
Sharif Olorin <sio@tesser.org>
2016-06-09 05:59:07 +0000

    Verify request MAC

--- src/Zodiac/Symmetric.hs
+++ src/Zodiac/Symmetric.hs
@@ -3,6 +3,7 @@
 module Zodiac.Symmetric(
     authenticationString
   , macRequest
+  , verifyRequest
   ) where
 
 import           Data.ByteString (ByteString)
@@ -11,10 +12,12 @@ import qualified Data.Text.Encoding as T
 
 import           P
 
+import           System.IO (IO)
+
 import           Tinfoil.Data (HashFunction, renderHashFunction, renderHash)
-import           Tinfoil.Data (MAC, SymmetricKey, keyHashFunction)
+import           Tinfoil.Data (Verified, MAC, SymmetricKey, keyHashFunction)
 import           Tinfoil.Hash (hash)
-import           Tinfoil.MAC (macBytes)
+import           Tinfoil.MAC (macBytes, verifyMAC)
 
 import           Zodiac.Data
 import           Zodiac.MAC
@@ -34,6 +37,22 @@ macRequest TSRPv1 hf kid rts re cr sk =
       authString = authenticationString TSRPv1 hf kid rts re cr
       khf = keyHashFunction hf in
   macBytes khf authKey authString
+
+-- | Verify a request MAC.
+verifyRequest :: SymmetricProtocol
+              -> HashFunction
+              -> KeyId
+              -> RequestTimestamp
+              -> RequestExpiry
+              -> CRequest
+              -> SymmetricKey
+              -> MAC
+              -> IO Verified
+verifyRequest TSRPv1 hf kid rts re cr sk mac =
+  let authKey = deriveRequestKey TSRPv1 (timestampDate rts) kid sk
+      authString = authenticationString TSRPv1 hf kid rts re cr
+      khf = keyHashFunction hf in
+  verifyMAC khf authKey authString mac
 
 -- | Construct a string to authenticate, from all the information which will
 -- be included in the request MAC.
