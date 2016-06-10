5121bae01ec63950987253ce90efc9432b1febaa
Sharif Olorin <sio@tesser.org>
2016-06-10 05:57:32 +0000

    Construct http-client auth header

--- src/Zodiac/TSRP/HttpClient.hs
+++ src/Zodiac/TSRP/HttpClient.hs
@@ -2,27 +2,47 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Zodiac.TSRP.HttpClient(
     macHttpClientRequest
+  , httpAuthHeader
   ) where
 
+import qualified Data.CaseInsensitive as CI
+
 import           Network.HTTP.Client (Request)
+import           Network.HTTP.Types (Header)
 
 import           P
 
 import           Tinfoil.Data (HashFunction(..), MAC, SymmetricKey)
 
 import           Zodiac.Data
+import           Zodiac.Request
 import           Zodiac.Request.HttpClient
 import           Zodiac.Symmetric
 
-macHttpClientRequest :: KeyId
+httpAuthHeader :: SymmetricProtocol
+               -> HashFunction
+               -> KeyId
+               -> RequestTimestamp
+               -> RequestExpiry
+               -> CRequest
+               -> MAC
+               -> Header
+httpAuthHeader sp hf kid rt re cr mac =
+  let sh = signedHeaders cr
+      sah = SymmetricAuthHeader sp hf kid rt re sh mac in
+  (CI.mk "Authorization", renderSymmetricAuthHeader sah)
+
+macHttpClientRequest :: SymmetricProtocol
+                     -> HashFunction
+                     -> KeyId
                      -> SymmetricKey
                      -> RequestExpiry
                      -> Request
                      -> RequestTimestamp
                      -> Either RequestError MAC
-macHttpClientRequest kid sk re r rts = do
+macHttpClientRequest sp hf kid sk re r rts = do
   cr <- toCanonicalRequest r
-  pure $ macRequest TSRPv1 SHA256 kid rts re cr sk
+  pure $ macRequest sp hf kid rts re cr sk
   
 
 
