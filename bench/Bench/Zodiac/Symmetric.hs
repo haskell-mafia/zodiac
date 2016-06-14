469fcc021f6582f2732b509d0affd0a90634a075
Sharif Olorin <sio@tesser.org>
2016-06-14 06:02:34 +0000

    Add bench skeleton + benchmark macRequest

---
+++ bench/Bench/Zodiac/Symmetric.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Bench.Zodiac.Symmetric where
+
+import           Bench.Zodiac
+
+import           Criterion.Main
+
+import           P
+
+import           Test.QuickCheck (arbitrary)
+import           Test.Zodiac.Arbitrary ()
+
+import           Zodiac.Data
+import           Zodiac.Symmetric
+
+macRequestEnv = (,,,,) <$> arbitrary
+                       <*> arbitrary
+                       <*> arbitrary
+                       <*> arbitrary
+                       <*> arbitrary
+
+benchmarks = [
+    env (generate' macRequestEnv) $ \ ~(kid, rt, re, cr, sk) ->
+      bgroup "Symmetric" $ [bench "macRequest" $ nf (macRequest TSRPv1 kid rt re cr) sk]
+  ]
