14840f41d895abe2d2cf9fffb75f055bf87a2fac
Sharif Olorin <sio@tesser.org>
2016-07-01 05:30:30 +0000

    zodiac-raw: fill out interface in root module

--- zodiac-raw/src/Zodiac/Raw.hs
+++ zodiac-raw/src/Zodiac/Raw.hs
@@ -7,4 +7,37 @@ This module is the primary interface for signing and verifying raw HTTP
 TSRP requests with zodiac and should provide everything needed to do so.
 -}
 module Zodiac.Raw(
+    KeyId
+  , SymmetricKey
+  , SymmetricProtocol(..)
+
+  , parseSymmetricKey
+  , parseSymmetricProtocol
+  , renderKeyId
+  , renderSymmetricKey
+  , renderSymmetricProtocol
+
+  -- * Key generation
+  , genKeyId
+  , genSymmetricKey
+
+  -- * Timestamps
+  , timestampRequest
+
+  -- * raw HTTP interface
+  , authedRawRequest
+  , rawKeyId
+  , verifyRawRequest
+  , verifyRawRequest'
+  , RequestError(..)
+  , renderRequestError
   ) where
+
+import           Tinfoil.Data.Key (SymmetricKey, parseSymmetricKey, renderSymmetricKey)
+
+import           Zodiac.Core.Data.Key
+import           Zodiac.Core.Data.Protocol
+import           Zodiac.Core.Key
+import           Zodiac.Core.Time
+import           Zodiac.Raw.Error
+import           Zodiac.Raw.TSRP
