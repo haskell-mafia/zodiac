2526af7ec2419006d107fcc0939d44c256342211
Sharif Olorin <sio@tesser.org>
2016-07-04 02:26:42 +0000

    zodiac-cli: param/command types

---
+++ zodiac-cli/src/Zodiac/Cli/TSRP/Data.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Zodiac.Cli.TSRP.Data(
+    TSRPCommand(..)
+  , TSRPParams(..)
+  ) where
+
+import           Data.ByteString (ByteString)
+
+import           P
+
+import           Zodiac.Raw
+
+data TSRPCommand =
+    TSRPAuth !TSRPParams !RequestExpiry !ByteString
+  | TSRPVerify !TSRPParams !ByteString
+
+-- | Parameters required for auth/verification.
+--
+-- These are things which would normally be provided via environment
+-- variables on *nix, but due to potential need to support Windows we're
+-- keeping this abstract interface.
+data TSRPParams = TSRPParams {
+    tsrpSecretKey :: !SymmetricKey
+  , tsrpKeyId :: !KeyId
+  } deriving (Eq)
