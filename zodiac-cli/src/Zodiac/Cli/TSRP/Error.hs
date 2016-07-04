09e375d45d9a71c52109f95390d2540cd242477b
Sharif Olorin <sio@tesser.org>
2016-07-04 05:52:05 +0000

    zodiac-cli: parse environment variables

--- zodiac-cli/src/Zodiac/Cli/TSRP/Error.hs
+++ zodiac-cli/src/Zodiac/Cli/TSRP/Error.hs
@@ -8,6 +8,8 @@ module Zodiac.Cli.TSRP.Error(
   , renderTSRPError
   ) where
 
+import qualified Data.Text as T
+
 import           P
 
 data TSRPError =
@@ -18,10 +20,14 @@ renderTSRPError (TSRPParamError e) = renderParamError e
 
 data ParamError =
     MissingRequiredParam !Text
-  | InvalidParam !Text
+  | InvalidParam !Text !Text
 
 renderParamError :: ParamError -> Text
 renderParamError (MissingRequiredParam p) =
   "missing required parameter: " <> p
-renderParamError (InvalidParam p) =
-  "invalid parameter: " <> p
+renderParamError (InvalidParam var val) = T.unwords [
+    "invalid parameter"
+  , var
+  , ": failed to parse"
+  , val
+  ]
