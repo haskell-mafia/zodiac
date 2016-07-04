54173ad4b2272871937c3354a936866ee8b16774
Sharif Olorin <sio@tesser.org>
2016-07-04 06:03:17 +0000

    zodiac-cli: read/validate HTTP requests

---
+++ zodiac-cli/src/Zodiac/Cli/Request.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Zodiac.Cli.Request(
+    readHTTPRequest
+  ) where
+
+import           Control.Monad.IO.Class (liftIO)
+
+import qualified Data.ByteString as BS
+
+import           Hadron (HTTPRequest)
+
+import           P
+
+import           System.IO (IO)
+
+import           X.Control.Monad.Trans.Either (EitherT, hoistEither)
+
+import           Zodiac.Raw
+import           Zodiac.Raw.Request
+
+readHTTPRequest :: EitherT RequestError IO HTTPRequest
+readHTTPRequest =
+  hoistEither . parseRawRequest =<< liftIO BS.getContents
