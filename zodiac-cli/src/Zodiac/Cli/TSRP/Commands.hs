e3424a5afd2e6a9b05191032db5ecc3429749871
Sharif Olorin <sio@tesser.org>
2016-07-04 22:54:59 +0000

    zodiac-cli: implement `tsrp authenticate`

--- zodiac-cli/src/Zodiac/Cli/TSRP/Commands.hs
+++ zodiac-cli/src/Zodiac/Cli/TSRP/Commands.hs
@@ -6,12 +6,19 @@ module Zodiac.Cli.TSRP.Commands(
   , verify
   ) where
 
+import           Control.Monad.IO.Class (liftIO)
+
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+
+import           P
 
 import           System.IO (IO)
 
-import           X.Control.Monad.Trans.Either (EitherT, left)
+import           X.Control.Monad.Trans.Either (EitherT, left, firstEitherT, hoistEither)
 
+import           Zodiac.Cli.TSRP.Data
+import           Zodiac.Cli.TSRP.Env
 import           Zodiac.Cli.TSRP.Error
 import           Zodiac.Raw
 
@@ -19,7 +26,11 @@ import           Zodiac.Raw
 -- authenticated request to stdout. Key ID and secret key are read
 -- from the environment.
 authenticate :: RequestExpiry -> EitherT TSRPError IO ByteString
-authenticate _re = left TSRPNotImplementedError
+authenticate re = do
+  (TSRPParams sk kid) <- tsrpParamsFromEnv
+  rt <- liftIO timestampRequest
+  bs <- liftIO BS.getContents
+  firstEitherT TSRPRequestError . hoistEither $ authedRawRequest kid sk re bs rt
 
 -- | Read an authenticated HTTP request on stdin and verify it with
 -- the key ID and secret key provided in the environment.
