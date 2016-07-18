a58cb5272b1cfad3b1645e44a7a99a2521aaddca
Sharif Olorin <sio@tesser.org>
2016-07-18 03:18:06 +0000

    zodiac-core: export tsrp_verify

--- zodiac-export/src/Zodiac/Export.hs
+++ zodiac-export/src/Zodiac/Export.hs
@@ -5,14 +5,22 @@
 module Zodiac.Export(
   )where
 
-import           Foreign.C.Types (CUChar)
+import           Foreign.C.Types (CTime(..), CChar(..), CUChar(..), CSize(..), CInt(..))
 import           Foreign.Ptr (Ptr)
 
 import           System.IO (IO)
 
 import           Zodiac.Export.Key
+import           Zodiac.Export.TSRP
 
 foreign export ccall "_z_tsrp_gen_key_id" genKeyId :: Ptr CUChar -> IO ()
 
 foreign export ccall "_z_tsrp_gen_symmetric_key" genSymmetricKey :: Ptr CUChar -> IO ()
+
+foreign export ccall "_z_tsrp_verify" verifyRawRequest' :: Ptr CChar
+                                                        -> Ptr CChar
+                                                        -> Ptr CChar
+                                                        -> CSize
+                                                        -> CTime
+                                                        -> IO CInt
 
