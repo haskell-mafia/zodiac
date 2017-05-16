cfa8c764e6933181c4b82c89a0c51842e904827a
Sharif Olorin <sio@tesser.org>
2017-05-16 21:58:26 +0000

    http-client: test for empty path canonicalisation

--- zodiac-http-client/test/Test/Zodiac/HttpClient/Request.hs
+++ zodiac-http-client/test/Test/Zodiac/HttpClient/Request.hs
@@ -6,6 +6,8 @@ module Test.Zodiac.HttpClient.Request where
 import           Disorder.Core.Run (ExpectedTestSpeed(..), disorderCheckEnvAll)
 import           Disorder.Core.Tripping (tripping)
 
+import qualified Network.HTTP.Client as HC
+
 import           P
 
 import           System.IO (IO)
@@ -19,6 +21,19 @@ import           Zodiac.HttpClient.Request
 
 prop_tripping_CRequest :: CRequest -> Property
 prop_tripping_CRequest = tripping fromCanonicalRequest toCanonicalRequest
+
+prop_toCanonicalRequest_empty_path:: CRequest -> Property
+prop_toCanonicalRequest_empty_path r =
+  let
+    r0 = fromCanonicalRequest r
+    r1 = r0 { HC.path = "" }
+    r2 = r0 { HC.path = "/" }
+    r' = r { crURI = CURI "/" }
+  in
+  conjoin [
+      toCanonicalRequest r1 === toCanonicalRequest r2
+    , toCanonicalRequest r1 === Right r'
+    ]
 
 return []
 tests :: IO Bool
